dc6674a936ed10b24e167d4cbae625edb1f08bba
Md. Ahsanur Rahaman <mdahsanurrahaman2456@gmail.com>
2025-03-01 01:38:56 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example of a simple blog carousel functionality (if you want to add more interactivity)
+document.addEventListener('DOMContentLoaded', () => {
+    const blogsContainer = document.querySelector('.blogs-slider-container');
+    
+    // Implement carousel or slider functionality here (if needed)
+  });
+  
